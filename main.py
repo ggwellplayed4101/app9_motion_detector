1f8baaca84cabed80dc04821c4c5b6218d9b3f6c
Bhargav Chataut <bhargavchataut101@gmail.com>
2025-03-22 19:30:28 -0500

    Add code to record first frame

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import time
 video = cv2.VideoCapture(0)
 time.sleep(1)
 
+first_frame = None
+
 while True:
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -11,6 +13,9 @@ while True:
 
     cv2.imshow("My video", gray_frame_gau)
 
+    if first_frame is None:
+        first_frame = gray_frame_gau
+        
     key = cv2.waitKey(1)
 
     if key == ord("q"):
