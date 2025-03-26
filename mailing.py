2d18984620afc9e1c295a8c19528f1dab4059075
Bhargav Chataut <bhargavchataut101@gmail.com>
2025-03-26 16:52:20 -0500

    feat add send email with attached image

--- mailing.py
+++ mailing.py
@@ -1,2 +1,35 @@
-def send_email():
-    print("Email was sent")
+import smtplib
+import imghdr
+from email.message import EmailMessage
+
+# Google password for this app
+PASSWORD = "mwlf xkug lyse eyax"
+SENDER = "ggwellplayed4101@gmail.com"
+RECIEVER = "bhargavchataut101@gmail.com"
+
+def send_email(image_path):
+    # Create object which can mail attachments 
+    email_message = EmailMessage()
+    email_message["Subject"] = "New customer showed up!"
+    email_message.set_content("Hey we just saw a new customer!")
+
+    # Read binary info of the image
+    with open(image_path, "rb") as file:
+        content = file.read()
+
+    # Subtype refers to metadata in an image 
+    email_message.add_attachment(content, 
+                                 maintype="image", 
+                                 subtype=imghdr.what(None, content))
+    
+    
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECIEVER, email_message.as_string())
+    gmail.quit()
+
+
+if __name__ == "__main__":
+    send_email(image_path="images/7.png")
